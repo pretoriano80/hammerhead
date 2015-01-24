arter97/caf_msm-5.4

msm: mdss: add support for different panel power states
Currently a panel either be in a powered on state or a powered off
state. However, it is possible for panels to be configured in an
intermediate low-power state where the panel is always kept on. In
order to support such use-cases, it is necessary to add the concept
of panel power states in MDSS so that each software layer can
independently maintain it's state and manage transitions between them.
This change just refactors the driver with only two existing power
states - panel on and panel off.

Change-Id: I49d1fda506761546c5e22d9106c5177e75053a81
Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>

drivers/video/msm/mdss/dsi_host_v2.c
@@ -1230,7 +1230,7 @@ static int msm_dsi_cont_on(struct mdss_panel_data *pdata)
 			goto error_vreg;
 		}
 	}
-	pinfo->panel_power_on = 1;
+	pinfo->panel_power_on = MDSS_PANEL_POWER_ON;
 	ret = mdss_dsi_panel_reset(pdata, 1);
 	if (ret) {
 		pr_err("%s: Panel reset failed\n", __func__);

pretoriano80/hammerhead

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

drivers/video/msm/mdss/dsi_host_v2.c
@@ -1255,7 +1255,7 @@ static int msm_dsi_cont_on(struct mdss_panel_data *pdata)
 			goto error_vreg;
 		}
 	}
-	pinfo->panel_power_on = 1;
+	pinfo->panel_power_state = MDSS_PANEL_POWER_ON;
 	ret = mdss_dsi_panel_reset(pdata, 1);
 	if (ret) {
 		pr_err("%s: Panel reset failed\n", __func__);